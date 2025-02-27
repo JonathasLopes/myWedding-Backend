2cf8ec3a1d81fec3e408cf79a8d82c69def37afb
JonathasLopes <jon.athas@hotmail.com>
2025-02-27 00:00:24 -0300

    messages added

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ import { storage } from './Configurations/Multer';
 import InvitesController from './Controllers/InvitesController';
 import fs from 'fs';
 import path from 'path';
+import MessageController from './Controllers/MessageController';
 
 const router = Router();
 
@@ -13,17 +14,18 @@ const router = Router();
 //   fs.mkdirSync(uploadDir);
 // }
 
-// const upload = multer({ storage });
+//const upload = multer({ storage });
 
 router.get("/", (req, res) => { res.send("Estou on!") });
 
 router.get("/GetAllInvites", InvitesController.GetAllInvites);
+router.get("/GetAllMessages", MessageController.GetAll);
 
 //router.post('/uploadInvites', upload.single('file'), InvitesController.UploadExcel);
 router.post('/SearchInvite', InvitesController.SearchInvite);
+router.post('/SendMessage', MessageController.SendMessage);
 
-
-
-//router.delete('/DeleteAllInvites', InvitesController.DeleteAllInvites);
+router.delete('/DeleteAllInvites', InvitesController.DeleteAllInvites);
+router.delete('/DeleteMessage', MessageController.DeleteMessage);
 
 export default router;
