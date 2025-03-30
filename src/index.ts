6925b56e801c06001a0b356f25fc4c9d6ab3bb78
JonathasLopes <jon.athas@hotmail.com>
2025-03-30 13:27:39 -0300

    scheduler removed

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import express from 'express';
 import cors from 'cors';
 import router from './routes';
 import dotenv from 'dotenv';
-import "./scheduler";
 
 const app = express();
 dotenv.config();
