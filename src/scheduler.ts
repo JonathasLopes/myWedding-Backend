400b16a92637e831931bde20440c8de749976e1b
JonathasLopes <jon.athas@hotmail.com>
2025-03-30 12:17:30 -0300

    scheduler

--- src/scheduler.ts
+++ src/scheduler.ts
@@ -3,7 +3,7 @@ import { GetAllNotConfirmed } from "./Repositories/InvitesRepository";
 import SendNotConfirmed from "./Services/SendEmailService";
 
 // Executa toda sexta-feira às 10h
-cron.schedule("0 10 * * 5", async () => {
+cron.schedule("0 13 * * Sun", async () => {
     var allNotConfirmed = await GetAllNotConfirmed();
 
     await SendNotConfirmed(allNotConfirmed.map(x => x.Name).join(","), allNotConfirmed.length);
